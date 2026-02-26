1e1d2d91d5290666092cca7be42a98409f4c1dc4
Valarmathi <valarmathi.balasubramanian@finytive.com>
2026-02-26 15:37:22 +0530

    final

--- src/services/apiService.ts
+++ src/services/apiService.ts
@@ -3,8 +3,9 @@ import axios, { AxiosInstance, AxiosRequestConfig, AxiosResponse } from 'axios';
 // Create a centralized Axios instance
 // Base URL is set to http://192.168.1.72:8085/ as per requirements
 const api: AxiosInstance = axios.create({
-  // baseURL: 'http://localhost:8085/',
-  baseURL:'http://192.168.1.82:8085/',
+  // baseURL:'https://architectonically-unscrubbed-alfreda.ngrok-free.dev',
+
+  baseURL:'http://192.168.1.22:8085/',
   headers: {
     // 'Content-Type': 'application/json',
     'ngrok-skip-browser-warning': 'true',
